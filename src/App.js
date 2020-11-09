8700270e803d2d53cf7f706cef8b26e1460935df
Rajat Beladiya <rajatbeladiya7@gmail.com>
2020-11-09 15:10:04 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { getERCContractInstance } from './utils';
+import web3 from './web3/web3';
+
+class App extends Component {
+  state = {
+    erc20Address: '0x0000000000000000000000000000000000000000',
+    spenderAddress: '0x0000000000000000000000000000000000000000',
+    amount: 0,
+    account: '',
+
+    // allowance
+    erc20AddressA: '0xFf795577d9AC8bD7D90Ee22b6C1703490b6512FD', // AAVE DAI address
+    spenderAddressA: '0x0000000000000000000000000000000000000000',
+    allowance: 0,
+  }
+
+  async componentDidMount() {
+    const [account] = await web3.eth.getAccounts();
+    this.setState({ account });
+  }
+  
+  onApproveClick = async () => {
+    const ercContract = await getERCContractInstance(web3, this.state.erc20Address);
+    await ercContract.methods.approve(this.state.spenderAddress, this.state.amount).send({ from: this.state.account });
+    alert('Approved Successfully!');
+  }
+
+  onCheckAllowanceClick = async () => {
+    const ercContract = await getERCContractInstance(web3, this.state.erc20AddressA);
+    const allowance = await ercContract.methods.allowance(this.state.account, this.state.spenderAddressA).call();
+    this.setState({ allowance });
+  }
+
+  hanldeState = (state) => {
+    this.setState(state);
+  }
+
+  render() {
+    return (
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}>
+        <h2>Approve</h2>
+        <div>ERC20 address</div>
+        <input type="text" onChange={event => this.hanldeState({ erc20Address: event.target.value })} />
+        <div>Spender address</div>
+        <input type="text" onChange={event => this.hanldeState({ spenderAddress: event.target.value })} />
+        <div>Amount</div>
+        <input type="text" onChange={event => this.hanldeState({ amount: event.target.value })} />
+        <div>
+          <button onClick={() => this.onApproveClick()}>Approve</button>
+        </div>
+
+        <h2>Allowance</h2>
+        <div>ERC20 address</div>
+        <input type="text" value={this.state.erc20AddressA} onChange={event => this.hanldeState({ erc20AddressA: event.target.value })} />
+        <div>Spender address</div>
+        <input type="text" onChange={event => this.hanldeState({ spenderAddressA: event.target.value })} />
+        <div>
+          <button onClick={() => this.onCheckAllowanceClick()}>Check Allowance</button>
+        </div>
+        <div>{`Allowance ${this.state.account} to ${this.state.spenderAddressA} is: ${this.state.allowance}`}</div>
+      </div>
+    )
+  }
+}
+
+export default App;
